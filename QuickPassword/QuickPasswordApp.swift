2638db8fe128932eb12b8407314da1fb02cc975e
Omar Leal <omarenriquelealr@gmail.com>
2023-05-06 10:51:29 -0400

    Initial Commit

---
+++ QuickPassword/QuickPasswordApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickPasswordApp.swift
+//  QuickPassword
+//
+//  Created by Omar Leal on 5/6/23.
+//
+
+import SwiftUI
+
+@main
+struct QuickPasswordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
